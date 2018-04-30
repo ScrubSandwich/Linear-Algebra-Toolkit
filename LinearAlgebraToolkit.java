3b474515c8052c1ccc6841ef4ec91c8f70265d45
jmieczni <jmieczni@purdue.edu>
2018-04-29 23:53:26 -0400

    Complete Determinant functionality

--- LinearAlgebraToolkit.java
+++ LinearAlgebraToolkit.java
@@ -41,9 +41,50 @@ public class LinearAlgebraToolkit {
     return m;
   }
 
-  // public static int determinant() {
+  public static int determinant(int[][] m) {
+    int n = m.length;
 
-  // }
+    if (n == 1) {
+      return (m[0][0]);
+    }
+
+    if (n == 2) {
+      return ((m[0][0] * m[1][1]) - (m[0][1] * m[1][0]));
+    }
+
+    int sum = 0;
+
+    for (int j = 0; j < n; j++) {
+      int[][] m2 = new int[n - 1][n - 1];
+
+      // Populate the smaller matrix with the correct values from the original
+      for (int k = 0; k < m2.length; k++) {
+
+        int h = 0;
+        for (int p = 0; p < n; p++) {
+
+          // If we are at the jth column, don't add anyhthing to the matrix
+          if (p != j) {
+            m2[k][h++] = m[k + 1][p];
+          }
+        }
+      }
+
+      sum += ( (getSign(j) * m[0][j]) * determinant(m2) );
+    }
+
+    return sum;
+  }
+
+  // Return +1 if i is even
+  // Return -1 if i is odd
+  public static int getSign(int i) {
+    if (i % 2 == 0) {
+      return 1;
+    }
+
+    return -1;
+  }
 
   public static void main(String[] args) {
     while (true) {
@@ -51,14 +92,17 @@ public class LinearAlgebraToolkit {
 
       int input = reader.nextInt();
 
-      if (input == 1) {
-        //determinant(matrix);
-
-        int[][] matrix = getMatrixInput();
-        printMatrix(matrix);
-      } else {
+      if (input == 99) {
         System.out.println("Goodbye");
         System.exit(0);
+      }
+
+      int[][] matrix = getMatrixInput();
+      
+      if (input == 1) {
+        System.out.println();
+        System.out.println("Determinant of the " + matrix.length + "x" + matrix.length + " matrix:");
+        System.out.println(determinant(matrix));
       }
     }    
   }
